319b4be6f3663eb1306d8ec9aec6ffa9c1cb2175
gjt <1163236754@qq.com>
2018-07-13 01:54:38 +0800

    新增XML解析工具，半成品

--- framework/helpers/tools/basePublicTools.php
+++ framework/helpers/tools/basePublicTools.php
@@ -12,8 +12,37 @@
  */
 class basePublicTools{
 
-
-    function  XMLUtils($str){
+    /**
+     * xml解析工具（暂时不启用，缺少高效读取SQL方法）
+     * @param $filePath
+     */
+    private function  SQLXMLUtil($filePath){
+        $con = file_get_contents($filePath);
+        /**
+         * XML文件标签
+         */
+        $xmlTag = array(
+            "SQL",
+            "SQL_type",
+            "SQL_method",
+            "SQL_content"
+        );
+        $arr = array();
+        /**
+         * 遍历标签
+         */
+        foreach($xmlTag as $x){
+            preg_match_all("/<".$x.">.*<\/".$x.">/", $con, $temp);
+            $arr[] = $temp[0];
+        }
+        $data = array();
+        foreach($arr as $key => $value) {
+            foreach($value as $k => $v) {
+                $a = explode($xmlTag[$key].'>', $v);
+                $v = substr($a[1], 0, strlen($a[1])-2);
+                $data[$k][$xmlTag[$key]] = $v;
+            }
+        }
 
     }
 }
